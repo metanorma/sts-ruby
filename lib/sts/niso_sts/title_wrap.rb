1c50c3d49c0eb459cdf7a769b0d56b8f049e09c1
Ronald Tse <ronald.tse@ribose.com>
2024-05-13 08:46:38 +0800

    fix: title/full can contain `std` elements, but it may be more than one (TODO)...

--- lib/sts/niso_sts/title_wrap.rb
+++ lib/sts/niso_sts/title_wrap.rb
@@ -3,14 +3,34 @@
 require_relative "../mapper"
 
 require_relative "reference_list"
+require_relative "reference_standard"
 
 module Sts
   module NisoSts
+    class TitleFull < Sts::Mapper
+      attribute :content, Shale::Type::String
+      attribute :std, ReferenceStandard
+
+      xml do
+        root "full"
+        map_content to: :content
+        map_element "std", to: :std, namespace: nil, prefix: nil
+      end
+    end
+
+    class TitleCompl < TitleFull
+      xml do
+        root "compl"
+        map_content to: :content
+        map_element "std", to: :std, namespace: nil, prefix: nil
+      end
+    end
+
     class TitleWrap < Sts::Mapper
       attribute :intro, Shale::Type::String
       attribute :main, Shale::Type::String
-      attribute :full, Shale::Type::String
-      attribute :compl, Shale::Type::String
+      attribute :full, TitleFull
+      attribute :compl, TitleCompl
       attribute :lang, Shale::Type::String
 
       xml do
