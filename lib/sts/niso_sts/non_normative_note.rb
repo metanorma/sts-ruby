9b8fab3353849c73b5290cc94058784771c858e3
HassanAkbar <hassanakbar2@gmail.com>
2024-05-13 18:58:35 +0500

    Finalized parsing of adoptions

--- lib/sts/niso_sts/non_normative_note.rb
+++ lib/sts/niso_sts/non_normative_note.rb
@@ -8,12 +8,16 @@ module Sts
   module NisoSts
     class NonNormativeNote < Sts::Mapper
       attribute :id, Shale::Type::String
+      attribute :content_type, Shale::Type::String
       attribute :p, Paragraph
       attribute :label, Shale::Type::String
 
       xml do
         root "non-normative-note"
+
+        map_attribute "content-type", to: :content_type
         map_attribute "id", to: :id
+
         map_element "p", to: :p
         map_element "label", to: :label
       end
